817cbc5e0b37f4ff06453a24cbaf08d1fd17e6c4
Huzhangjie <1534634433@qq.com>
2017-11-18 01:36:15 +0800

    使用node后台执行js 使用babel 将es6转换为es5 --watch实时监听文件的改变 使用 实现字符串和参数的合并。

---
+++ es6/template_string_compiled.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// const name = 'Snackers';
+// const age = '2';
+
+// const dog = `My dog is ${name},${age} years old.`;
+// console.log(dog);
+
+// 将数据变成界面
+// const beer = {
+//     name: 'Belgian Wit',
+//     brewery: 'Steam Whistle Brewery',
+//     keyword: ['pale','cloudy','spiced','crisp']
+// };
+
+// function renderKeywords(keywords) {
+//     return `
+//         <ul>
+//            ${keywords.map(keyword => 
+//               `<li>${keyword}</li>`).join('')}
+//         </ul>
+//     `
+// }
+
+// const markup = `
+//     <div class="beer">
+//         <h2>${beer.name}</h2>
+//         <p class="brewery">${beer.brewery}</p>
+//         ${renderKeywords(beer.keyword)}
+//     </div>
+// `;
+// // 可声明的HTML标记 运行在JS
+// document.body.innerHTML = markup;
+
+var dogs = [{ name: '小白', age: 2 }, { name: '黑', age: 2 }, { name: '小飞', age: 3 }];
+// map 函数, 属于dogs 数组
+// iterator 迭代 全部查询了一遍
+// 箭头函数 dog为参数 一个参数  括号可省
+// 函数只有一句代码 可省大括号 return
+document.body.innerHTML = dogs.map(function (dog) {
+    return (
+        // console.log(dog)
+        '\n        <li>' + dog.name + ' is ' + dog.age + ' years old</li>\n    '
+    );
+}).join('');
+// .join方法 将数组推平 元素一个一个列出
